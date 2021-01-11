72fba76821c0c29cb9e46ddacf146a99d7d3c3c0
SedanaYoga <9g.yoga27@gmail.com>
2021-01-11 16:44:47 +0700

    First commit, done creating frontend, connecting to firestore

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+
+const Chat = () => {
+  const [input, setInput] = useState("");
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    console.log("You typed >>>", input);
+    setInput("");
+  };
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at ...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className={`chat__message ${false && "chat__receiver"}`}>
+          <span className="chat__name">Sedana Yoga</span>
+          Hey Guys
+          <span className="chat__timestamp">2:08PM</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="type a message..."
+          />
+          <button type="submit" onClick={sendMessage}></button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0gT8S3pLaQ8SFLgXRR4ngK7zWX1JS55M",
+  authDomain: "whatsapp-clone-515a0.firebaseapp.com",
+  projectId: "whatsapp-clone-515a0",
+  storageBucket: "whatsapp-clone-515a0.appspot.com",
+  messagingSenderId: "1042742209583",
+  appId: "1:1042742209583:web:00dad1066a106e93a26ada",
+  measurementId: "G-54RNK31C4W",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
